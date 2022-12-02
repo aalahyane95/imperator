f026718627ca200ed8d20f50f9901464ad193d1c
Abdellatif Alahyane <abdellatif.dev@gmail.com>
2022-12-02 16:29:58 +0100

    Fix chain-id

--- cmd/root.go
+++ cmd/root.go
@@ -142,7 +142,7 @@ func NewRootCmd(
 
 	rootCmd := &cobra.Command{
 		Use:   appName + "d",
-		Short: "Imperator has a imperator sort of love for ICS",
+		Short: "Imperator the blockchain powered by ICS",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
